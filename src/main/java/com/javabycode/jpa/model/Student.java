86073d467c5ce47366891c367d77cffe7fb1ab13
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-10 08:32:21 +0700

    #4 - Add Student Model

---
+++ src/main/java/com/javabycode/jpa/model/Student.java
@@ -0,0 +1,38 @@
+package com.javabycode.jpa.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+    private final String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_detail_id")
+    private StudentDetail studentDetail;
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public Student(String name, StudentDetail studentDetail) {
+        this.name = name;
+        this.studentDetail = studentDetail;
+    }
+}
+
+

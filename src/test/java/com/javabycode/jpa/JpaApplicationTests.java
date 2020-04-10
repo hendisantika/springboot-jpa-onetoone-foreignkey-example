d6e14a6e9b2d2d5b940d14ae999870e9d08ca0c2
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-10 08:26:30 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/javabycode/jpa/JpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.javabycode.jpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = MyJPAApplication.class)
+public class JpaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

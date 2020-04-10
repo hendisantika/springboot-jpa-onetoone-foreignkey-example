682fdb084c0cf8650956fb8a98b1bce22286ec28
Hendi Santika <hendisantika@yahoo.co.id>
2020-04-10 08:33:24 +0700

    #6 - Add StudentRepository

---
+++ src/main/java/com/javabycode/jpa/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.javabycode.jpa.repository;
+
+import com.javabycode.jpa.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<Student, Integer> {
+}
